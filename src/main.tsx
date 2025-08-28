81e511492cc3b520b86bd293f02efaa87cb56a0f
lauamat <101799633+lauamat@users.noreply.github.com>
2025-08-28 10:35:11 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import HabitsGate from './components/shared/HabitsGate'  // ← esta ruta
+import SessionRefresher from './components/shared/SessionRefresher'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HabitsGate>
-      <App />
-    </HabitsGate>
+    <SessionRefresher />
+    <App />
   </React.StrictMode>,
 )
