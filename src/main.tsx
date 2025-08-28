d040727602fed48cc4a2122ad229d61e00f899b7
lauamat <101799633+lauamat@users.noreply.github.com>
2025-08-28 09:25:39 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import HabitsGate from './components/shared/HabitsGate'  // ← esta ruta
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HabitsGate>
+      <App />
+    </HabitsGate>
   </React.StrictMode>,
 )
